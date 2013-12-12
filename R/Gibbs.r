92751421a143c1b8376df34e5b883421b1ffadf0
Yifan <siiowl@users.noreply.github.com>
2013-12-12 11:03:16 -0500

    Update Gibbs.r

--- R/Gibbs.r
+++ R/Gibbs.r
@@ -1,24 +1,29 @@
-A = matrix(rep(0.998,50^2),ncol=50)*outer(1:50,1:50,'*')
-diag(A)=(1:50)^2
-
-m=10000
-x0=c(1:50)
-
+Gibbs.Norm50<-function(ITER=10000,p=50,chains=4){
+A = matrix(rep(0.998,p^2),ncol=p)*outer(1:p,1:p,'*')
+diag(A)=(1:p)^2
+m=ITER
+x0=rep(0,p)
 n = length(x0)
-
-
-store = matrix(0,n,m)
-store[,1] = x0
-for (i in 2:m){
-xt = store[,(i-1)]
- for (j in 1:n){
-  mu = 0 + A[j,-j]%*%solve(A[-j,-j])%*%(xt[-j])
-  sigma = A[j,j]-A[j,-j]%*%solve(A[-j,-j])%*%A[-j,j]
-  store[j,i] = rnorm(1,mu,sqrt(sigma))
- }
+Gibbs.lst<-list()
+length(Gibbs.lst)=chains
+tt=0
+for (ii in 1:chains) {
+  tic<-proc.time()
+  store = matrix(0,n,m)
+  store[,1] = x0
+  for (i in 2:m){
+    xt = store[,(i-1)]
+    for (j in 1:n){
+      mu = 0 + sum((A[j,-j]%*%solve(A[-j,-j]))*(xt[-j]))
+      sigma = A[j,j]-A[j,-j]%*%solve(A[-j,-j])%*%A[-j,j]
+      store[j,i] = rnorm(1,mu,sqrt(sigma))
+    }
+  }
+  toc<-proc.time()
+  t0=toc-tic
+  Gibbs.lst[[ii]]=t(store)
+  tt=t0+tt
 }
-X11()
-plot(store[1,],store[2,],type="l")
-
-
+return(Gibbs.lst);
+}
 
