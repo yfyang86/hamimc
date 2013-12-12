a4464e4aa0f7212f36f8ba8510816b7b280ec790
Yifan <siiowl@users.noreply.github.com>
2013-12-12 10:16:36 -0500

    Update MH.framework.R

--- R/MH.framework.R
+++ R/MH.framework.R
@@ -22,31 +22,43 @@ return(X.flow);
 }
 
 
+MH.Normal<-function(ITER=10000,p=50,x.init=rep(0,50),
+                    Sigma=matrix(rep(0.998,p^2),ncol=p)*outer(1:p,1:p,'*')+0.002*diag((1:p)^2),
+                    chains=4,
+                    buinin=5000
+                    ){
+                    
+if (buinin>(ITER/2)) stop('Buin-in should be > 2 total iterations');
+if ((dim(Sigma)[1])!=(dim(Sigma)[2])) stop('Sigma should be a possitive definet square matrix!(We will not detect >0 assumption)');
+if(dim(Sigma)[1]!=p) stop('Dim(Sigma) should be p');
+if (length(x.init)!=p) stop('Initial value should be of length p');
+if (chains<1) stop('Number of chains should be >=1');
+chains=ceiling(chains);
 
-#####################
-# usage
-#####################
-#ITER=10000
-#x.initial=1:50
-#buinin=5000
-#dproposalfunction<-function(xt1,xt){# We could try other proposals
-#  mvtnorm::dmvnorm(xt1,mean=xt,sigma=diag(rep(1,length(xt)))/20,log=T)
-#}
 
-#rproposalfunction<-function(xt){
-#  mvtnorm::rmvnorm(n=1,mean=xt,sigma=diag(rep(1,length(xt)))/20)
-#}
+dproposalfunction<-function(xt1,xt){# We could try other proposals
+  mvtnorm::dmvnorm(xt1,mean=xt,sigma=diag(rep(1,length(xt)))/20,log=T)
+}
 
-##target function
-#A = matrix(rep(0.998,50^2),ncol=50)*outer(1:50,1:50,'*')
-#diag(A)=(1:50)^2
+rproposalfunction<-function(xt){
+  mvtnorm::rmvnorm(n=1,mean=xt,sigma=diag(rep(1,length(xt)))/20)
+}
 
-#dtarget<-function(x,...){
-#  p=length(x)
-#  mvtnorm::dmvnorm(x,mean=rep(0,p),sigma=A,log=T)
-#}
+dtarget<-function(x,...){
+  p=length(x)
+  mvtnorm::dmvnorm(x,mean=rep(0,p),sigma=Sigma,log=T)
+}
 
-#   MH.framework(dproposalfunction,rproposalfunction,dtarget,A=A) ->  X.flow
+re<-list();
+length(re)=chains;
+
+for(i in 1:chains){
+cat('\nChain ',i,' running...\n')
+  MH.framework(dproposalfunction,rproposalfunction,dtarget,ITER,50,x.init,Sigma=Sigma) ->  re[[i]]
+}
+
+return (re);
+}
 
 #library(coda)
 
