fdb1667a4bb0035ef3bec2010d4cc4640d24f0b7
Yifan <yfyang86@users.noreply.github.com>
2016-04-04 14:52:25 -0400

    Release 0.0.011

--- src/mh.h
+++ src/mh.h
@@ -47,9 +47,6 @@ namespace biostacs
 
 	};
 
-	
-
-
 	class MetropolisMCMC :public Likelihood{
 	private:
 		double SumOfLogLik;
@@ -57,7 +54,6 @@ namespace biostacs
 		int iter;
 		int maxiter;
 		vector<double> StartingParam;
-		random_device rd;
 		vector<double> PriorParam;
 		vector<double> Proposal;
 		mat MonteCarloChain;
@@ -66,7 +62,7 @@ namespace biostacs
 		int ParamNum;		// p: # of params, redesign need to be dropped.
 		int PriorParamNum;		// p: # of params
 		mat Xval;
-		mh_rnd::default_rnd rnd;
+		mh_rnd::rnorm rnd;
 		double sumloglik(double pdf(vector<double> param, mat x), vector<double>param) {
 			SumOfLogLik = 0.;
 			for (int i = 0; i < SampleSizeNum; i++) {
@@ -74,7 +70,20 @@ namespace biostacs
 			}
 			return(SumOfLogLik);
 		}
-
+        void ProgressBar(int progress_int) {
+			double progressPct = double(progress_int) / maxiter;
+			int count = 0;
+			cout << "[";
+			int barWidth =50;
+			int pos = barWidth * progressPct;
+			for (int i = 0; i < barWidth; ++i) {
+				if (i < pos) std::cout << "=";
+				else if (i == pos) std::cout << ">";
+				else std::cout << " ";
+			}
+			cout << "] " << int(progressPct * 100.0)<< "%\r";
+			cout.flush();
+		}
 	public:
 		double priorlog(double priorpdf(vector<double> param), vector<double>param) {
 			SumOfPriorLogLik = priorpdf(param);
@@ -91,50 +100,61 @@ namespace biostacs
 		MetropolisMCMC(vector<double> parameters, mat &data, 
 			int MaxIterations, vector<double> startingvalue) :
 				Likelihood(parameters,data),
-				maxiter(MaxIterations),StartingParam(startingvalue),
-				PriorParamNum(startingvalue.size()){
+				StartingParam(startingvalue)
+				{
+			SampleSizeNum = data.n_rows;
+			ParamNum = data.n_cols;
+			Xval = data;
+			iter = 0;
+			PriorParamNum = startingvalue.size();
+			maxiter=MaxIterations;
 		};
 
-		void normal_proposal(vector<double> param) {
-			for (size_t i = 0; i < PriorParamNum; i++) {
-				Proposal[i] = rnd.rproposal()*.1 + abs(param[i]);
+		void normal_proposal(vector<double> param) { // ~ N(param, .2), i.e.: param+.2*rnorm(1)
+			for (int i = 0; i < PriorParamNum; i++) {
+				Proposal[i] = rnd.r()*.2 + param[i];
 			}
 		}
 
 		mat MH_MCMC(double pdf(vector<double> param, mat x), double priorpdf(vector<double> param)) {
-			iter = 0;
+			
 			MonteCarloChain.reshape(maxiter+1, PriorParamNum); // 1+Maxiter * Num of Params
 			Proposal.resize(PriorParamNum);
-			
+			std::random_device rda;
+			std::mt19937 gen(rda());
+			std::uniform_real_distribution<> runif(0, 1);
+
 			for (int i = 0; i < PriorParamNum; i++) { 
 				MonteCarloChain(0, i) = StartingParam[i]; 
 			}
 			vector<double> tmpVec;
 			tmpVec.resize(PriorParamNum);
 			double ProbDiff;
+            int progressSEP=maxiter/50;
 			for (int i = 1; i <= maxiter; i++) {
-				if (i % 100 == 0) cout << "Pct: " << 100.*i / maxiter << "%\t Acc rate: " << 100.*iter/i <<'%'<<endl;
+                if (i % progressSEP == 0)ProgressBar(i);
+#ifdef DEBUG
+				if (i % 1000 == 0) cout << i << "-run\tPct: " << 100.*i / maxiter << "%\t Acc rate: " << 100.*iter/i <<'%'<<endl;
+#endif
+
 				for (int jj = 0; jj < PriorParamNum; jj++) tmpVec[jj] = MonteCarloChain(i - 1, jj);
 				
 				normal_proposal(tmpVec); // proposal
 
 				ProbDiff = posterior(pdf, priorpdf, Proposal) - posterior(pdf, priorpdf, tmpVec);
 		
-				if (log(rnd.runif()) < ProbDiff) {
+				if (log(runif(gen)) < ProbDiff) {
 					for (int ii = 0; ii < PriorParamNum; ii++)  MonteCarloChain(i, ii) = Proposal[ii];
 					iter++;
 				}
 				else {
 					for (int jj = 0; jj < PriorParamNum; jj++) MonteCarloChain(i, jj) = MonteCarloChain(i - 1, jj);
 				}
-				/* check step by step
-				cout<<'\n' << i << "-th run:";
-				for (double ss : tmpVec)  cout<< ss << '\t';
-				system("pause");
-				*/
 			}
 				return(MonteCarloChain);
 			}
 		};
+
+
 
 	}
